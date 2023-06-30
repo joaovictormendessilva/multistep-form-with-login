b2a63790b9292cb5c15841f6809bf8b714d60c1f
João Victor <joao_victor-08@hotmail.com>
2023-06-30 10:26:13 -0300

    Modificado parametros da interface NewUser

--- src/interfaces/NewUser.ts
+++ src/interfaces/NewUser.ts
@@ -1,6 +1,6 @@
-import { User } from "./User";
-
-export interface NewUser extends User {
+export interface NewUser {
   email: string;
+  username: string
+  password: string
   confirmPassword: string;
 }
