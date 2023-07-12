935a114fc184d8cd2b39582ba99f3094b28054cf
João Victor <joao_victor-08@hotmail.com>
2023-07-12 12:16:07 -0300

    feat: Criado hook para registro do user

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -11,30 +11,22 @@ import atIcon from "../../assets/at.svg";
 import eyeIcon from "../../assets/eye.svg";
 import slashEyeIcon from "../../assets/eye-slash.svg";
 
-// Interface
-import { NewUser } from "../../interfaces/NewUser";
-import { MessageProps } from "../../interfaces/Message";
-
 interface RegisterFormProps {
   setToggle: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 // Components
 import { SystemMessages } from "../SystemMessages/SystemMessages";
-
-// Axios
-import axios from "axios";
-
-const api = import.meta.env.VITE_API;
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 export function RegisterForm({ setToggle }: RegisterFormProps) {
-  const [changeNewUser, setChangeNewUser] = useState<NewUser>({} as NewUser);
+
+  const { changeNewUser, setChangeNewUser, message, handleCreateUser } = useCreateUser()
+
   const [showPassword, setShowPassword] = useState({
     type: "password",
     show: false,
   });
-
-  const [message, setMessage] = useState<MessageProps>({} as MessageProps)
 
   const handleChangeNewUser = (e: ChangeEvent<HTMLInputElement>) => {
     setChangeNewUser((prev) => ({
@@ -56,63 +48,6 @@ export function RegisterForm({ setToggle }: RegisterFormProps) {
         type: "password",
         show: false,
       }));
-    }
-  };
-
-  const handleCreateUser = async () => {
-    if (
-      changeNewUser.email &&
-      changeNewUser.username &&
-      changeNewUser.password &&
-      changeNewUser.confirmPassword
-    ) {
-      if (changeNewUser.password === changeNewUser.confirmPassword) {
-        await axios.post(`${api}/nova-conta/`, {
-          Username: changeNewUser.username,
-          Email: changeNewUser.email,
-          Password: changeNewUser.password,
-          ConfirmPassword: changeNewUser.confirmPassword,
-        }, {
-          headers: {
-            "Content-Type": 'application/json'
-          }
-        })
-          .then((response) => {
-
-            if (response.data.StatusCode === 200) {
-              setMessage((prevValue) => ({
-                ...prevValue,
-                message: response.data.Message,
-                type: response.data.StatusCode
-              }))
-            }
-
-            setChangeNewUser((prevValue) => ({
-              ...prevValue,
-              username: '',
-              email: '',
-              password: '',
-              confirmPassword: ''
-            }))
-
-          })
-          .catch((error) => {
-            console.log(error.response.data)
-            if (error.response.data.StatusCode === 406) {
-              setMessage((prevValue) => ({
-                ...prevValue,
-                message: error.response.data.Message,
-                type: error.response.data.StatusCode
-              }))
-            }
-          });
-      }
-      else {
-        alert("As senhas não são iguais!");
-      }
-    }
-    else {
-      alert("Todos os campos são obrigatórios");
     }
   };
 
---
+++ src/hooks/useCreateUser.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { NewUser } from "../interfaces/NewUser";
+import axios from "axios";
+import { MessageProps } from "../interfaces/Message";
+
+const api = import.meta.env.VITE_API;
+
+export function useCreateUser() {
+  const [changeNewUser, setChangeNewUser] = useState<NewUser>({} as NewUser);
+  const [message, setMessage] = useState<MessageProps>({} as MessageProps)
+
+  const handleCreateUser = async () => {
+    if (
+      changeNewUser.email &&
+      changeNewUser.username &&
+      changeNewUser.password &&
+      changeNewUser.confirmPassword
+    ) {
+      if (changeNewUser.password === changeNewUser.confirmPassword) {
+        await axios.post(`${api}/nova-conta/`, {
+          Username: changeNewUser.username,
+          Email: changeNewUser.email,
+          Password: changeNewUser.password,
+          ConfirmPassword: changeNewUser.confirmPassword,
+        }, {
+          headers: {
+            "Content-Type": 'application/json'
+          }
+        })
+          .then((response) => {
+
+            if (response.data.StatusCode === 200) {
+              setMessage((prevValue) => ({
+                ...prevValue,
+                message: response.data.Message,
+                type: response.data.StatusCode
+              }))
+            }
+
+            setChangeNewUser((prevValue) => ({
+              ...prevValue,
+              username: '',
+              email: '',
+              password: '',
+              confirmPassword: ''
+            }))
+
+          })
+          .catch((error) => {
+            console.log(error.response.data)
+            if (error.response.data.StatusCode === 406) {
+              setMessage((prevValue) => ({
+                ...prevValue,
+                message: error.response.data.Message,
+                type: error.response.data.StatusCode
+              }))
+            }
+          });
+      }
+      else {
+        alert("As senhas não são iguais!");
+      }
+    }
+    else {
+      alert("Todos os campos são obrigatórios");
+    }
+  };
+
+  return { changeNewUser, setChangeNewUser, message, handleCreateUser }
+}
